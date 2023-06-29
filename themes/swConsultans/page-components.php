08edbb916be004d39d18d7de0f1897cf892c6723
Mario Germán Reyes <mager19@gmail.com>
2023-06-29 15:56:02 -0500

    page components added

---
+++ themes/swConsultans/page-components.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * The template for displaying all pages
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ * @package swConsultans
+ */
+
+get_header(); ?>
+
+<div class="container mx-auto">
+    <div class="flex">
+        <div class="w-5/12 bg-secondary-dark">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Esse perspiciatis minima, quibusdam exercitationem est ad,
+            recusandae facilis autem odit consequuntur reiciendis.
+            Placeat deserunt amet maxime delectus cumque animi
+            voluptatem nesciunt!
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+get_footer();
